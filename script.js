f6c9a30d58e0c62a0b59cddf52d584ff3ed50ebb
KatrinaZuus <katrinazuus@gmail.com>
2024-03-23 23:45:58 +0400

    map fiter

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ let container = document.querySelector(".container")
 let bg = document.querySelector(".bg")
 let darkBg = document.querySelector(".dark-bg")
 let leftClear = document.querySelector(".left-clear")
-let todo1 = document.querySelector("todo1")
 let listOfTodo = document.querySelector(".list-of-todo")
+let reorder = document.querySelector(".reorder")
+let all = document.querySelector(".all")
+let active = document.querySelector(".active")
+let complete = document.querySelector(".completed")
 
-// change theme
+
+// თემების შეცვლა
 moon.addEventListener("click", ()=>{
     bodythemeColor.classList.add("dark")
 })
@@ -18,259 +22,180 @@ sun.addEventListener("click", ()=>{
     bodythemeColor.classList.remove("dark")
 })
 
-// add todo
-
 let itemsQty = document.querySelector(".items-qty")
 let inputField = document.querySelector(".create-a-new-todo")
-let i = 1
-inputField.addEventListener('keypress', function (e) {
+
+
+
+
+
+
+    let addtop = -65
+    let items = 0
+    let leftCleartop = 65
+    let reordertop = 375
+    let containerheigth = 1024
+    let removeitem = 0
+
+    let todo=[]
+    let todoadd=[]
+    let todotext=[]
+    let todocross=[]
+    let todoedit=[]
+    let todooval=[]
+    let todocheck=[]
   
-    
-    if (e.key === 'Enter' && i===5 && inputField.value !== "") {
-        todoadd = document.createElement(`div`)
-        todoadd.classList.add("todo5") 
-        listOfTodo.appendChild(todoadd)
-
-        todotext = document.createElement(`div`)
-        todotext.classList.add("todotext")
-        todoadd.appendChild(todotext)
-        todotext.innerHTML=inputField.value
-
-        todocross = document.createElement(`div`)
-        todocross.classList.add("todocross")
-        todoadd.appendChild(todocross)
-        todocross.innerHTML = "X"
-
-        todooval = document.createElement(`div`)
-        todooval.classList.add("todooval")
-        todoadd.appendChild(todooval)
-
-        todocheck = document.createElement(`div`)
-        todocheck.classList.add("todocheck")
-        todooval.appendChild(todocheck)
-        todocheck.innerHTML = "v"
-       
-        inputField.value = ""
-        itemsQty.innerText = i
-        i=i+1   
-     } 
-     if (e.key === 'Enter' && i===4 && inputField.value !== "") {
-        todoadd = document.createElement(`div`)
-        todoadd.classList.add("todo4") 
-        listOfTodo.appendChild(todoadd)
-
-        todotext = document.createElement(`div`)
-        todotext.classList.add("todotext")
-        todoadd.appendChild(todotext)
-        todotext.innerHTML=inputField.value
-
-        todocross = document.createElement(`div`)
-        todocross.classList.add("todocross")
-        todoadd.appendChild(todocross)
-        todocross.innerHTML = "X"
-
-        todooval = document.createElement(`div`)
-        todooval.classList.add("todooval")
-        todoadd.appendChild(todooval)
-
-        todocheck = document.createElement(`div`)
-        todocheck.classList.add("todocheck")
-        todooval.appendChild(todocheck)
-        todocheck.innerHTML = "v"
-        
-        inputField.value = ""
-        itemsQty.innerText = i
-        i=i+1
-     } 
-
-     if (e.key === 'Enter' && i===3 && inputField.value !== "") {
-        todoadd = document.createElement(`div`)
-        todoadd.classList.add("todo3") 
-        listOfTodo.appendChild(todoadd)
-
-        todotext = document.createElement(`div`)
-        todotext.classList.add("todotext")
-        todoadd.appendChild(todotext)
-        todotext.innerHTML=inputField.value
-
-        todocross = document.createElement(`div`)
-        todocross.classList.add("todocross")
-        todoadd.appendChild(todocross)
-        todocross.innerHTML = "X"
-
-        todooval = document.createElement(`div`)
-        todooval.classList.add("todooval")
-        todoadd.appendChild(todooval)
-
-        todocheck = document.createElement(`div`)
-        todocheck.classList.add("todocheck")
-        todooval.appendChild(todocheck)
-        todocheck.innerHTML = "v"
-
-        inputField.value = ""
-        itemsQty.innerText = i
-        i=i+1
-     } 
-
-     if (e.key === 'Enter' && i===2 && inputField.value !== "") {
-        todoadd = document.createElement(`div`)
-        todoadd.classList.add("todo2") 
-        listOfTodo.appendChild(todoadd)
-        
-        todotext = document.createElement(`div`)
-        todotext.classList.add("todotext")
-        todoadd.appendChild(todotext)
-        todotext.innerHTML=inputField.value
-        
-        todocross = document.createElement(`div`)
-        todocross.classList.add("todocross")
-        todoadd.appendChild(todocross)
-        todocross.innerHTML = "X"
-
-        todooval = document.createElement(`div`)
-        todooval.classList.add("todooval")
-        todoadd.appendChild(todooval)
-
-        todocheck = document.createElement(`div`)
-        todocheck.classList.add("todocheck")
-        todooval.appendChild(todocheck)
-        todocheck.innerHTML = "v"
-        
-        inputField.value = ""
-        itemsQty.innerText = i
-        i=i+1 
-     } 
-
-     if (e.key === 'Enter' && i===1 && inputField.value !== "") {
-        todoadd = document.createElement(`div`)
-        todoadd.classList.add("todo1") 
-        listOfTodo.appendChild(todoadd)
-      
-        
-        todotext = document.createElement(`div`)
-        todotext.classList.add("todotext")
-        todoadd.appendChild(todotext)
-        todotext.innerHTML=inputField.value
-
-
-        // ასე დამატება არ მომწონს, img ვერ ჩავსვი
-        todocross = document.createElement(`div`)
-        todocross.classList.add("todocross")
-        todoadd.appendChild(todocross)
-        todocross.innerHTML = "X"
-
-        todooval = document.createElement(`div`)
-        todooval.classList.add("todooval")
-        todoadd.appendChild(todooval)
-
-        todocheck = document.createElement(`div`)
-        todocheck.classList.add("todocheck")
-        todooval.appendChild(todocheck)
-        todocheck.innerHTML = "v"
-
-        inputField.value = ""
-        itemsQty.innerText = i
-        i=i+1
-     } 
-
-     newoval = Array.from(document.querySelectorAll(".todooval"))
-     checked = document.querySelectorAll(".todocheck")
-     console.log(newoval[0])
-    // ამ ფერზე ვერ დავსვი
-    // `linear-gradient (135deg, #5df, #c058f3)` 
-
-    
-     
-    
-    let  checkedone = 0   
-    one = newoval[0]
-    
-    one.addEventListener("click", ()=>{
-        if (checkedone == 1){
-        one.style.backgroundColor = "#fff"
-        checkedone = 0
-            } else {
-                
-        if (checkedone == 0){
-            one.style.backgroundColor = "#c058f3"
-            // todotext.style.textDecoration = "none"
-            checkedone = 1                 
-            }
-        }
-        })
-
-    
-    let  checkedtwo = 0   
-    two = newoval[1]
-    
-    two.addEventListener("click", ()=>{
-        if (checkedtwo == 1){
-        two.style.backgroundColor = "#fff"
-        // two.style.textDecoration = "underline"
-        checkedtwo = 0
-            } else {
-                
-        if (checkedtwo == 0){
-            two.style.backgroundColor = "#c058f3"
-            // two.style.textDecoration = "none"
-            checkedtwo = 1                 
-            }
-        }
-        })
-
-    let  checkedthree = 0   
-    three = newoval[2]
-    
-    three.addEventListener("click", ()=>{
-        if (checkedthree == 1){
-        three.style.backgroundColor = "#fff"
-        // three.style.textDecoration = "underline"
-            } else {
-                
-        if (checkedthree == 0){
-            three.style.backgroundColor = "#c058f3"
-            // three.style.textDecoration = "none"
-            checkedthree = 1                 
-            }
-        }
-        })
-
-
-    let  checkedfour = 0   
-    four = newoval[3]
-        
-    four.addEventListener("click", ()=>{
-        if (checkedfour == 1){
-        four.style.backgroundColor = "#fff"
-        // four.style.textDecoration = "underline"
-            } else {
-                    
-        if (checkedfour == 0){
-            four.style.backgroundColor = "#c058f3"
-            // four.style.textDecoration = "none"
-            checkedfour = 1                 
-            }
-            }
-            })
-
-        
-               
-    let  checkedfive = 0   
-        five = newoval[4]
+//  todo ლისტების დამატება
+        inputField.addEventListener('keypress', function (e) {
             
-        five.addEventListener("click", ()=>{
-            if (checkedfive == 1){
-            five.style.backgroundColor = "#fff"
-            // five.style.textDecoration = "underline"
-                } else {
+            if (e.key === 'Enter' && inputField.value !== "") {
+                items = items + 1
+                leftCleartop = leftCleartop + 65 
+                leftClear.style.top = `${leftCleartop}px`
+
+                reordertop = reordertop + 65
+                reorder.style.top = `${reordertop}px`
+
+                containerheigth = containerheigth + 65
+                container.style.height = `${containerheigth}px`
+  
+                todoadd = document.createElement(`div`)
+                todoadd.classList.add("todo") 
+                listOfTodo.appendChild(todoadd)
+                addtop = addtop+65
+                todoadd.style.top = `${addtop}px`
+                
+                
+                todotext = document.createElement(`div`)
+                todotext.classList.add("todotext")
+                todoadd.appendChild(todotext)
+                todotext.innerHTML=inputField.value 
+
+                todocross = document.createElement(`div`)
+                todocross.classList.add("todocross")
+                todoadd.appendChild(todocross)
+                // აქ როგორ დავამატო img?? "X" არ მომწონს
+                todocross.innerHTML = "X"
+
+
+                todooval = document.createElement(`div`)
+                todooval.classList.add("todooval")
+                todoadd.appendChild(todooval)
+        
+                todocheck = document.createElement(`div`)
+                todocheck.classList.add("todocheck")
+                todooval.appendChild(todocheck)
+                todocheck.innerHTML = "v"
+
+                
+                todoedit = document.createElement(`div`)
+                todoedit.classList.add("todoedit")
+                todoadd.appendChild(todoedit)
+                todoedit.innerHTML = "undo"
+                
+                
+        
+                inputField.value = ""
+                
+                // items = items + 1
+                // itemsQty.innerText = `${items}`
+
+                
+                todo.push ({
+                    maindiv: todoadd,
+                    text: todotext,
+                    edit: todoedit,
+                    cross: todocross,
+                    oval: todooval,
+                    check: todocheck,
+                    completed: false,
+                    deleted: false,
+                    
+                    })
+                    
+//  todo ლისტების მინიშვნა, აღდგენა და წაშლა
+                    
+
+                    // აქ გაფილტვრის და ამოშლის შემთხვევაში ვერ ვაკეთებ todo-ებს შორის მანძილი რომ გავაქრო
+                    todo.map((each)=>{
                         
-            if (checkedfive == 0){
-                five.style.backgroundColor = "#c058f3"
-                // five.style.textDecoration = "none"
-                checkedfive = 1                 
-                }
-            }
-            })
+                        each.oval.addEventListener("click", ()=>{
+                            each.oval.style.backgroundImage = "linear-gradient(135deg, #5df, #c058f3)"
+                            each.text.style.textDecoration = "line-through"
+                            each.check.style.display = "block"
+                            each.completed = true
+                        }) 
+                        
+                        each.edit.addEventListener("click", ()=>{
+                            each.check.style.display = "none"
+                            each.text.style.textDecoration = "none"
+                            each.oval.style.backgroundImage = "none"
+                            each.completed = false
+                        })
+                        
+                     // ამოშლის დროს todo-ესბი რაოდენობას ვერ ვაკლებ და ამოშლილ todo-ს შორის ადგილს ვერ ვაუქმებ
+                        each.cross.addEventListener("click", ()=>{
+                            if (each.completed == true){
+                            each.maindiv.style.display = "none"
+                            each.completed = null
+                            removeitem = removeitem + 1
+                            console.log(removeitem)
+                            // addtop = addtop-65
+                            // each.maindiv.nextSibling.style.top = `${addtop}px` 
+                            }     
+
+                        })  
+                            
+                              
+                    })
+                   
+
+                    // items = items + 1
+
+                    itmeNumber = items - removeitem
+                    console.log(itmeNumber)
+                itemsQty.innerText = `${itmeNumber}`
+
+// todo ლისტების გაფილტვრა
+                    active.addEventListener("click", ()=>{
+                        todo.filter((each)=>{
+                            if (each.completed == true) {
+                                each.maindiv.style.display = "none"
+                            }
+                            if (each.completed == false) {
+                                each.maindiv.style.display = "flex"
+                            }
+                        })
+                    })
+
+
+                    complete.addEventListener("click", ()=>{
+                        todo.filter((each)=>{
+                            if (each.completed == false) {
+                                each.maindiv.style.display = "none"
+                            }
+                            if (each.completed == true) {
+                                each.maindiv.style.display = "flex"
+                            }
+                        })
+                    })
+
+
+                    all.addEventListener("click", ()=>{
+                        todo.filter((each)=>{
+                            if (each.completed == false || each.completed == true) {
+                                each.maindiv.style.display = "flex"
+                                // each.oval.style.display = ""
+                            }
+                        })
+                    })
+
+
+                    
+
+        }
+
+    })
 
 
 
@@ -279,14 +204,6 @@ inputField.addEventListener('keypress', function (e) {
 
 
 
-
-
-
-
-
-
-
-            })
 
 
 
